f21fb736e1f154c8c9a5af84b7de382d3af802b9
idoski <valaniftbuisness@gmail.com>
2024-09-09 02:03:41 -0700

    Day save made

--- apppanel/interface.py
+++ apppanel/interface.py
@@ -10,46 +10,47 @@ class Interface(ctk.CTk):
         self.geometry("1200x900")
         self.title("Bargain Bros LLC")
         newday_button = ctk.CTkButton(master=self, text="New Day", command=lambda: self.new_day())
-        newday_button.pack(pady=12, padx=10)
+        newday_button.pack(pady=5, padx=5, anchor="nw")
         self.days = {}
         
     
     # Create day function
     def new_day(self):
-        day_object = day.Day(len(self.days))
-        tempPanel = panel.Panel(800,600,"New Day")
+        day_object = day.Day("Default")
+        print(day_object)
+        tempPanel = panel.Panel(1000,1000,"New Day")
+        tempPanel.focus()
         name = ctk.CTkEntry(master= tempPanel, placeholder_text="Day Number/Name")
         name.pack(pady=12, padx=10)
-            
-        add_button = ctk.CTkButton(master=tempPanel, text="Add Item", command=lambda: self.add(self))
-        add_button.pack(pady=12, padx=10)
         
-        save_day = ctk.CTkButton(master=tempPanel, text="Save", command=lambda: self.save(self, day_object))
+        i_name = ctk.CTkEntry(master= tempPanel, placeholder_text="Item name")
+        i_name.pack(pady=12, padx=10)
+        
+        i_amount = ctk.CTkEntry(master= tempPanel, placeholder_text="Amount")
+        i_amount.pack(pady=12, padx=10)
+        
+        save_i = ctk.CTkButton(master=tempPanel, text="Add Item", command=lambda: saving_i(self, day_object, i_name.get(), i_amount.get()))
+        save_i.pack(pady=12, padx=10)
+        
+        save_day = ctk.CTkButton(master=tempPanel, text="Save Day", command=lambda: self.save(day_object))
         save_day.pack(pady=12, padx=10)
-    
-    
-    # Add Function
-    def add(self, day_object):
-        tempPanel = panel.Panel(800,600,"Add Item")
-        name = ctk.CTkEntry(master= tempPanel, placeholder_text="Item name")
-        name.pack(pady=12, padx=10)
         
-        amount = ctk.CTkEntry(master= tempPanel, placeholder_text="Amount")
-        amount.pack(pady=12, padx=10)
-        
-        save_item = ctk.CTkButton(master=tempPanel, text="Save", command=lambda: save_it(self))
-        save_item.pack(pady=12, padx=10)
-        
-        def save_it(self):
-            day_object.create_item(name, amount)
-    
+        def saving_i(self, day, item, amount):
+            day.create_item(item, amount)
+            print("item saved!")
+
     
     def save(self, day_object):
         self.days[day_object.day_num] = day_object
         self.refresh()
         
     def refresh(self):
-        for item in self.days.items():
-            ctk.CTkLabel(master=self, text=item.day_num)
+        for item in self.days.values():
+            button = ctk.CTkButton(master=self, text="{item.day_num}", command=lambda: self.day_search(item.day_num))
+            button.pack(pady=5, padx=5, anchor="nw")
+            print("day saved")
+            
+    def day_search(day_name):
+        pass
     
     
--- day.py
+++ day.py
@@ -3,7 +3,6 @@ class Day():
     def __init__(self, day_num) -> None:
         super().__init__()
         self.day_num = day_num
-        self.num_items = 0
         self.locked = False
         self.items = {}
         
